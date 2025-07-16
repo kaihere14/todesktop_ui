71ecf78cabec4b6d8a344099990a7c05ab543728
kaihere14 <yashpal.sharma9950@gmail.com>
2025-07-16 15:55:43 +0530

    scroll animation

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const bar = document.querySelector('.bar');
 const i = document.querySelector('.bar i')
 const menue = document.querySelector('.mob-menue');
-
+const line1 = document.querySelector('.line1')
+const line2 = document.querySelector('.line2')
+const line3 = document.querySelector('.line3')
 let view = true;
 
 bar.addEventListener('click',()=>{
@@ -27,3 +29,30 @@ window.addEventListener('resize',()=>{
        i.classList.remove("fa-xmark")
     }
 })
+
+
+function setUpIntersectionObserver(element, isLTR, speed) {
+    const intersectionCallback = (entries) => {
+        const isIntersecting = entries[0].isIntersecting;
+        if(isIntersecting){
+            document.addEventListener('scroll',scrollHandler)
+        }else{
+            document.removeEventListener('scroll',scrollHandler)
+        }
+    };
+    const observer = new IntersectionObserver(intersectionCallback);
+    observer.observe(element);
+
+    function scrollHandler(){
+        const translateX = (window.innerHeight - element.getBoundingClientRect().top)*speed;
+        if(isLTR){
+        element.style.transform= `translateX(${translateX}px)`
+        }else{
+            element.style.transform= `translateX(-${translateX}px)`
+        }
+    }
+}
+
+setUpIntersectionObserver(line1, true, 0.15);
+setUpIntersectionObserver(line2, false, 0.15);
+setUpIntersectionObserver(line3, true, 0.15);
